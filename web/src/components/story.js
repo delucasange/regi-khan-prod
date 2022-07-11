456816647ffab0b2602555930b8fa8987ad80536
delucasange <delucas.angelina@gmail.com>
2022-07-11 17:23:59 +0200

    updated aspect ratio on videos

--- web/src/components/story.js
+++ web/src/components/story.js
@@ -1,6 +1,7 @@
 import React from "react"
 const Story = ({ videoSrcURL, videoTitle, ...props }) => (
+  <div class="aspect-w-9 aspect-h-16">
   <iframe className="storyStyle" src={videoSrcURL} title={videoTitle} frameborder="0" allow="accelerometer; fullscreen; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
-
+  </div>
 )
 export default Story
--- web/src/components/video.js
+++ web/src/components/video.js
@@ -1,7 +1,8 @@
 import React from "react"
 
 const Video = ({ videoSrcURL, videoTitle, ...props }) => (
+  <div class="aspect-w-16 aspect-h-9">
   <iframe className="videoStyle" src={videoSrcURL} title={videoTitle} frameborder="0" allow="accelerometer; fullscreen; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
-
+  </div>
 )
 export default Video
