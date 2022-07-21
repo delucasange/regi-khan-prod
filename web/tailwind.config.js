3b39ced9351b6e9355a7fb6ea771d40073af1fef
delucasange <delucas.angelina@gmail.com>
2022-07-21 20:59:12 +0200

    updated font in CSS styles and corrected global css

--- web/tailwind.config.js
+++ web/tailwind.config.js
@@ -20,16 +20,14 @@ module.exports = {
     borderRadius: {
       'none': '0',
       'sm': '0.125rem',
-      // DEFAULT: '0.25rem',
-      // DEFAULT: '4px',
       'md': '0.375rem',
       'lg': '0.5rem',
       'full': '200px',
       'large': '12px',
     },
     fontFamily: {
-      favorit: 'favorit',
-      goodsans:'goodsans',
+      light: 'light',
+      folio:'folio',
       times:'times',
     },
     extend: { }
